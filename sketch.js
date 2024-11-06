bbdc2b4466a35e140c36ce2abefb6a1b03ac6db0
Biel009xs <107139661+Biel009xs@users.noreply.github.com>
2024-11-06 15:31:27 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(800, 800, WEBGL); //plano 3D
+  createCanvas(1920, 1080, WEBGL); //plano 3D
   terra=loadImage('terra.jpg')
 }
 
@@ -7,7 +7,7 @@ function draw() {
   background(1000, 1000, 1000); //cor de fundo 
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(120, 140); //esfera 3D
+  sphere(200, 220); //esfera 3D
   
   
 }
